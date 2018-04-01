830219de82a7578e15a3a1427b7196d8d21910e6
bigsteve <usstefanmiller@yahoo.com>
2018-04-01 14:55:44 -0500

    implementation refactoring
    Declared varialbes;
    Responsive polygin draw corrections;
    Added createEl routine function;

--- src/dom/MapSelection.js
+++ src/dom/MapSelection.js
@@ -26,14 +26,18 @@
  
  
  
- 
 function MapSelection(mapObject, canv) {
     
     var mapContainer = mapObject._container;
+    var mapMenu;
     var consoleDebug = false;
     var canvasEl;
+    var buttonDraw;
+    var buttonDelete;
+    var canvasOffset = new Object();
     makeCanvas();
-    var countPoints = 0; countAreas = 1;
+    var countPoints = 0;
+    var countAreas = 1;
     var mapType = "leaflet";
     var cWidth = document.documentElement.clientWidth;
     var cHeight = document.documentElement.clientHeight;
@@ -43,7 +47,6 @@ function MapSelection(mapObject, canv) {
     var showCanvasLine = false;
     var captureCanvasImage = false;
     var points = [];
-    var areas = {"features": []};
     var areaShape = "polygon";
     var areaName = null;
     var pointX;
@@ -53,17 +56,17 @@ function MapSelection(mapObject, canv) {
     var tickness = 10;
     var shapeTickness = 3;
     var color = "200, 70, 0";
-    var drawButtonColor = "100,0,0";
     
     var isTouchSupported = "ontouchstart" in window;
     var clickEvent = isTouchSupported ? "tap" : "click";
     var startEvent = isTouchSupported ? "touchstart" : "mousedown";
     var moveEvent = isTouchSupported ? "touchmove" : "mousemove";
     var endEvent = isTouchSupported ? "touchend" : "mouseup";
+    var areas;
     var selectedArea;
-    var areaCoordinates = [];
+    var areaCoordinates;
     var mW, mH, N, E, S, W, zoom, maxZoom, bounds;
-    
+    setAreaObjects();
     
     
     
@@ -81,6 +84,16 @@ function MapSelection(mapObject, canv) {
     
     
     
+    function setAreaObjects() {
+        areas = {"features": []};
+        selectedArea;
+        areaCoordinates = [];
+    }
+    
+    
+    
+    
+   
     function makeCanvas(){
         
         if(mapType = "leaflet") {
@@ -93,56 +106,94 @@ function MapSelection(mapObject, canv) {
             zoom = mapObject.getZoom();
             maxZoom = mapObject.getMaxZoom();
             bounds = mapObject.getBounds();
-        }
-            
-        if(canv != undefined) {
-            canvasEl = canv;
-        } else {
-            canvasEl = document.createElement("canvas");
-            document.body.insertBefore(canvasEl, mapContainer);
-            buttonDraw = document.createElement("a");
-            buttonDraw.innerHTML = "S";
-            document.getElementsByClassName("leaflet-control")[0].appendChild(buttonDraw);
+            mapMenu = mapObject.zoomControl._container;
         }
         
-        canvasEl.style.position   = "absolute";
-        canvasEl.style.left   = "0";
-        canvasEl.style.top   = "0";
-        buttonDraw.style.lineHeight   = "40px";
-        buttonDraw.style.height   = "40px";
-        buttonDraw.style.fontWeight   = "bold";
-        buttonDraw.style.color   = "rgba("+drawButtonColor+", 1)";
-        buttonDraw.setAttribute("class", "leaflet-control-draw-area");
-        buttonDraw.setAttribute("href", "#");
-        canvasEl.width = window.innerWidth;
-        canvasEl.height = window.innerHeight;
+        
+        
+        if(canv != undefined) {
+            
+            canvasEl = canv;
+            
+        } else {
+            
+            canvasEl = createEl("canvas", {"id":"canvas-"+ mapContainer.id,"style":"position:absolute;left:"+mapContainer.offsetLeft + "px;top:"+mapContainer.offsetTop + "px;","width":mapContainer.clientWidth+";display:none;background:rgba(25,130,215,.2);z-index:999;","height":mapContainer.clientHeight}, mapContainer.parentNode,"");
+            
+            buttonDraw = createEl(
+                "a",
+                {"class":"leaflet-control-draw-area","style":"line-height:40px; height:40px;font-weight:bold; cursor:pointer;"},
+                mapMenu,
+                "S"
+            );
+        
+            buttonDelete = createEl(
+                "a",
+                {"class":"leaflet-control-draw-area","style":"line-height:40px; height:40px;font-weight:bold; cursor:pointer;"},
+                mapMenu,
+                "D"
+            );
+        
+     
+        }
+        
         
         if (mapContainer != undefined) {
-            canvasEl.width = mapContainer.clientWidth;
             canvasEl.style.display = "none";
-            canvasEl.height = mapContainer.clientHeight;
-            canvasEl.style.left   = mapContainer.offsetLeft + "px";
-            canvasEl.style.top   = mapContainer.offsetTop + "px";
+            canvasEl.style.background   = "rgba(25,130,215,.2)";
         }
+        
         
         mapContainer.getElementsByClassName("leaflet-top leaflet-left")[0].style.zIndex = 1000;
         canvasEl.style.zIndex   = 999;
     }
     
     
-    function setCanvas(){
+    
+    
+    
+    
+    function createEl(tagName, attr, appendTo, innerH) {
         
+        var el = document.createElement(tagName);
+        
+        for (var index in attr) {
+            
+            if (!attr.hasOwnProperty(index)) {
+                continue;
+            }
+            
+            el.setAttribute(index, attr[index]);
+        }
+        
+        el.innerHTML = innerH;
+        appendTo.appendChild(el);
+        
+        return el;
+    }
+    
+    
+    
+    
+    
+    function setCanvas(){
+        getElementOffset(mapContainer);
         canvasEl.addEventListener(startEvent, pointerDown, false);
         canvasEl.addEventListener(moveEvent, pointerMove, false);
         canvasEl.addEventListener(endEvent, pointerUp, false);
         buttonDraw.addEventListener(startEvent, useCanvas, false);
+        buttonDelete.addEventListener(startEvent, deleteSelections, false);
         ctx.lineWidth = tickness;
         ctx.lineJoin = ctx.lineCap = "round";
         ctx.shadowBlur = 2;
         ctx.shadowColor = "rgb("+color+", 1)";
         ctx.strokeStyle = "rgb("+color+")";
-        ctx.fillStyle = "rgba("+color+", .06)";
+        ctx.fillStyle = "rgba("+color+", .06)";    
+        canvasEl.width = mapContainer.clientWidth;
+        canvasEl.height = mapContainer.clientHeight;
     }
+    
+
+    
     
     setCanvas();
     
@@ -153,9 +204,29 @@ function MapSelection(mapObject, canv) {
             buttonDraw.style.color   = "rgba(255,0,0,1)";
         } else {
             canvasEl.style.display = "none";
-            buttonDraw.style.color   = "rgba("+drawButtonColor+", 1)";
+            buttonDraw.style.removeProperty("color");
         }
     }
+    
+    
+    
+    
+    
+    
+    function deleteSelections() {
+        
+        var paths = mapContainer.getElementsByTagName("path");
+        var l = paths.length;
+        
+        for(var i = 0; i < l; i++) {
+            paths[0].remove();
+        }
+        
+        setAreaObjects();
+    }
+    
+    
+    
     
     
     
@@ -163,6 +234,11 @@ function MapSelection(mapObject, canv) {
     window.onresize = function(e){
         setCanvas();
     };
+    
+    
+    onElementHeightChange(document.body, function(){
+       setCanvas();
+    });
  
     
     
@@ -198,7 +274,7 @@ function MapSelection(mapObject, canv) {
 
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         points.push({ x: pointX, y: pointY });
-        ppp = mapObject.layerPointToLatLng([pointX - mapObject._mapPane._leaflet_pos.x,pointY - mapObject._mapPane._leaflet_pos.y]);
+        var ppp = mapObject.layerPointToLatLng([pointX - mapObject._mapPane._leaflet_pos.x,pointY - mapObject._mapPane._leaflet_pos.y]);
         getCoordinates(ppp.lat, ppp.lng);
         
         if(showCanvasLine){
@@ -256,18 +332,73 @@ function MapSelection(mapObject, canv) {
     
     
     
-   
+      
+    function getElementOffset(el) {
+        
+        var ely = el;
+        
+        
+        var _offsetLeft = 0;
+        
+        do {
+          if ( !isNaN( el.offsetLeft )){
+              
+              _offsetLeft += el.offsetLeft;
+          }
+          
+        } while( el = el.offsetParent );
+        
+        
+        var _offsetTop = 0;
+        
+        do {
+          if (!isNaN( ely.offsetTop)){
+              
+              _offsetTop += ely.offsetTop;
+          }
+          
+        } while(ely = ely.offsetParent);
+        
+        canvasOffset.x = _offsetLeft;
+        canvasOffset.y = _offsetTop;
+        
+        
+    }
     
     
     
+    /**
+    * function created by vsync
+    * https://stackoverflow.com/users/104380/vsync
+    *
+    */
+    function onElementHeightChange(el, callback){
+        
+        var lastHeight = el.clientHeight, newHeight;
+        
+        (function run(){
+            newHeight = el.clientHeight;
+            if( lastHeight != newHeight )
+                callback();
+            lastHeight = newHeight;
+
+            if( el.onElementHeightChangeTimer )
+                clearTimeout(el.onElementHeightChangeTimer);
+
+            el.onElementHeightChangeTimer = setTimeout(run, 200);
+            
+        })();
+    }
+    
+
     
 
     
     function getPoints(e){
          pointX = (isTouchSupported) ? e.touches[0].pageX : e.pageX;
          pointY = (isTouchSupported) ? e.touches[0].pageY : e.pageY;
-         pointX = pointX - canvasEl.offsetLeft;
-         pointY = pointY - canvasEl.offsetTop;
+         pointX = pointX - canvasOffset.x;
+         pointY = pointY - canvasOffset.y;
     }
 
 
@@ -293,8 +424,8 @@ function MapSelection(mapObject, canv) {
     
     
     function coordinatesSummary(p){
-        o = [];
-        r = (p.length/processResolution > 0) ? Math.round(p.length/processResolution) : 1;
+        var o = [];
+        var r = (p.length/processResolution > 0) ? Math.round(p.length/processResolution) : 1;
         for(var i = 0; i < p.length; i++) {
             if(i%r === 0) {
                 o.push({ lat: p[i].lat, lng: p[i].lng });
@@ -328,7 +459,7 @@ function MapSelection(mapObject, canv) {
     
     function createArea(){
         if(selectedArea != null) mapObject.removeLayer(selectedArea);
-        shapeProp = [[areaCoordinates],{color: "rgba("+color+",1)", weight: shapeTickness}];
+        var shapeProp = [[areaCoordinates],{color: "rgba("+color+",1)", weight: shapeTickness}];
         switch(areaShape){
             case "polygon":    
                 selectedArea = (new L.Polygon(shapeProp[0], shapeProp[1])).addTo(mapObject).bindPopup(areaName);
@@ -338,7 +469,7 @@ function MapSelection(mapObject, canv) {
             break
         }
         
-        gjson = selectedArea.toGeoJSON();
+        var gjson = selectedArea.toGeoJSON();
         gjson.type = areaShape; 
         gjson.properties = {name:areaName, color: "rgba("+color+",1)", weight: shapeTickness};
         areas.features.push(gjson);
@@ -357,7 +488,7 @@ function MapSelection(mapObject, canv) {
 
         if(selectedArea != null) mapObject.removeLayer(selectedArea);
         
-        shapeProp = [[areaCoordinates],{color: "rgba("+color+",1)", weight: shapeTickness}];
+        var shapeProp = [[areaCoordinates],{color: "rgba("+color+",1)", weight: shapeTickness}];
         
         switch(areaShape){
             case "polygon":    
